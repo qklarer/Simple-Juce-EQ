8c0b809a7fa69134305cb1e9fcdc62f4667965ec
Quinn Klarer <84090326+qklarer@users.noreply.github.com>
2021-10-09 14:58:59 -0700

    Add files via upload
    Added Sliders to GUI

--- SimpleEQ/Source/PluginEditor.h
+++ SimpleEQ/Source/PluginEditor.h
@@ -11,6 +11,14 @@
 #include <JuceHeader.h>
 #include "PluginProcessor.h"
 
+struct CustomeRotarySlider : juce :: Slider
+{
+    CustomeRotarySlider() : juce::Slider(juce::Slider::SliderStyle::RotaryHorizontalVerticalDrag,juce::Slider::TextEntryBoxPosition::NoTextBox)
+    {
+        
+    }
+};
+
 //==============================================================================
 /**
 */
@@ -28,6 +36,16 @@ private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+    
+    CustomeRotarySlider peakFreqSlider,
+    peakGainSlider,
+    peakQualitySlider,
+    lowCutFreqSlider,
+    highCutFreqSlider,
+    lowCutSlopeSlider,
+    highCutSlopeSlider;
+    
+    std::vector<juce::Component*> getComps();
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
