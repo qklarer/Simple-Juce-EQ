8f9cddaed3caf093c19c9e181c4ddf009ce340ea
Quinn Klarer <84090326+qklarer@users.noreply.github.com>
2021-10-11 18:20:14 -0700

    Add files via upload
    Gave editor own mono chain.

--- SimpleEQ/Source/PluginEditor.cpp
+++ SimpleEQ/Source/PluginEditor.cpp
@@ -43,7 +43,7 @@ void SimpleEQAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+    g.drawFittedText ("", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void SimpleEQAudioProcessorEditor::resized()
